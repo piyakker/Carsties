932a8c81dc505d41f27f3373486c4a7c92956d14
Piyakker <ericlien86@gmail.com>
2024-05-12 17:44:29 +0800

    Add Auction Service

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+public class AuctionDbContext : DbContext
+{
+    public AuctionDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    // no need to add Item to DbSet because of 1:1 relationship, but remember to specify table name in plural!
+    public DbSet<Auction> Auctions { get; set; }
+}
